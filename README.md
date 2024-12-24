9bd57be17a37dd6b51fe395c1d4dcaae79af152d
Graham Ollis <plicease@cpan.org>
2024-12-23 22:43:25 -0700

    initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tie::Hash::DataSection ![static](https://github.com/uperl/Tie-Hash-DataSection/workflows/static/badge.svg) ![linux](https://github.com/uperl/Tie-Hash-DataSection/workflows/linux/badge.svg)
+
+Access \_\_DATA\_\_ section via tied hash
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2024 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Tie/Hash/DataSection.pm
+++ lib/Tie/Hash/DataSection.pm
@@ -1,9 +1,61 @@
 use warnings;
 use 5.020;
-use experimental qw( postderef signatures );
+use experimental qw( signatures );
+use stable qw( postderef );
 use true;
 
 package Tie::Hash::DataSection {
 
     # ABSTRACT: Access __DATA__ section via tied hash
+
+    use Data::Section::Pluggable 0.08;
+    use Ref::Util qw( is_plain_arrayref );
+
+    sub TIEHASH ($class, $package=undef, @plugins) {
+        $package //= caller;
+        my $dsp = Data::Section::Pluggable->new(
+            package => $package,
+        );
+        foreach my $plugin (@plugins) {
+            if(is_plain_arrayref $plugin) {
+                my($name, @args) = @$plugin;
+                $dsp->add_plugin($name => @args);
+            } else {
+                $dsp->add_plugin($plugin);
+            }
+        }
+        return bless [$dsp], $class;
+    }
+
+    sub FETCH ($self, $key) {
+        return $self->[0]->get_data_section($key);
+    }
+
+    sub EXISTS ($self, $key) {
+        exists $self->[0]->get_data_section->{$key};
+    }
+
+    sub FIRSTKEY ($self) {
+        $self->[1] = [keys $self->[0]->get_data_section->%*];
+        return $self->NEXTKEY;
+    }
+
+    sub NEXTKEY ($self, $=undef) {
+        return shift $self->[1]->@*;
+    }
+
+    sub STORE ($self, $, $) {
+        require Carp;
+        Carp::croak("hash is read-only");
+    }
+
+    sub DELETE ($self, $) {
+        require Carp;
+        Carp::croak("hash is read-only");
+    }
+
+    sub CLEAR ($self) {
+        require Carp;
+        Carp::croak("hash is read-only");
+    }
 }
